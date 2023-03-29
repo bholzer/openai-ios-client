72a88ebf609803b47999e0b85991a5c1b6485501
Brennan Holzer <brennan.holzer@gmail.com>
2023-03-28 21:04:42 -0500

    Initial Commit

---
+++ OpenAI Playground/OpenAI_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenAI_PlaygroundApp.swift
+//  OpenAI Playground
+//
+//  Created by Brennan Holzer on 3/28/23.
+//
+
+import SwiftUI
+
+@main
+struct OpenAI_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
